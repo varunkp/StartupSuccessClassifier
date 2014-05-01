df26c98a054376b5f4102e85d93e430f64f28b4c
pjloury <pjloury@gmail.com>
2014-05-01 14:57:52 -0700

    First successful Scraper plus Prelim data

--- crunch.py
+++ crunch.py
@@ -1,34 +1,103 @@
 import requests
 import time
-'''
-r = requests.get('http://warm-dawn-6399.herokuapp.com/diagnosis')
-rj= r.json()
-print rj['diagnosisText']
-'''
+import pandas as pd
+import os
+from bs4 import BeautifulSoup
+import simplejson
+import json
+import html2text
 
+fn = 'master3'
+data = pd.read_csv(fn+'.csv',error_bad_lines=False)
 api_key = 'ndcq6rwvpenbagu7p9rkxpw6'
-COMP_NAME='microsoft'
-start = time.time()
-print start
-#r = requests.get('http://api.crunchbase.com/v/1/company/box.js?api_key=ndcq6rwvpenbagu7p9rkxpw6')
-url1 = 'http://api.crunchbase.com/v/1/company/'+COMP_NAME+'.js?api_key='+api_key
-url2 = 'http://api.crunchbase.com/v/1/companies/permalink?name='+COMP_NAME+'&api_key='+api_key
 
-returned_json = requests.get(url1).json()
+#currentDir = os.path.dirname(os.path.abspath(__file__))
+#print currentDir
 
-end1 = time.time()
-print end1
+data['overview'] = " "
+data['tag_list'] = " "
+data["image1"] = " "
+data["image2"] = " "
+data["image3"] = " "
+data["number_of_employees"] = " "
+data["office_latitude"] = " "
+data["office_longitude"] = " "
+row = -1
 
-print returned_json['image']['available_sizes']
-#print returned_json['crunchbase_url']
-end = time.time()
+initial = time.time()
+for company in data['name']:
+  row = row+1
 
-print end1 - start
-print end - start
+  company = unicode(company, errors='ignore')
+  #company = "AB Group"
 
-#category_code
-'''
-json does not have 
-company_region
-company_category
-'''
+  startGET = time.time()
+  
+  url1 = 'http://api.crunchbase.com/v/1/company/'+company+'.js?api_key='+api_key
+  r = requests.get(url1)
+  if (r.status_code!= 200):
+    continue
+
+  returned_json = json.loads(r.text, strict= False)
+
+  #returned_json = r.json()
+  #print returned_json
+
+  if 'error' in returned_json:
+    continue
+
+  '''  
+  l = ['overview','tag_list','number_of_employees']
+  for jsonKey in l:
+    if returned_json[jsonKey] is None:
+      continue
+    data[jsonKey][row] = returned_json[jsonKey]
+    
+  if returned_json["image"] is not None and "image" in returned_json and len(returned_json["image"])>0:
+    data["image1"][row] =returned_json["image"]["available_sizes"][0][1]
+    data["image2"][row] =returned_json["image"]["available_sizes"][1][1]
+    data["image3"][row] =returned_json["image"]["available_sizes"][2][1]
+
+  if len(returned_json["offices"])>0 and returned_json["offices"] is not None:
+    data["office_latitude"][row] = returned_json["offices"][0]["latitude"]
+    data["office_longitude"][row] = returned_json["offices"][0]["longitude"]
+
+  endGET = time.time()
+  print company + " took " + str((endGET-startGET))
+
+  '''
+
+  if returned_json["overview"] is not None:   # and "overview" in returned_json
+    
+    cleanedHTML= BeautifulSoup((''.join(returned_json["overview"])))
+
+    text = html2text.html2text(cleanedHTML)
+    #text = html2text.html2text(returned_json["overview"])
+    print text  
+    data['overview'][row] = text
+
+
+  if returned_json["tag_list"] is not None: # and "tag_list" in returned_json
+    data['tag_list'][row] = returned_json["tag_list"]
+
+  if returned_json["number_of_employees"] is not None: # and "number_of_employees" in returned_json
+    data["number_of_employees"][row] = returned_json["number_of_employees"]
+
+  if returned_json["image"] is not None and "image" in returned_json and len(returned_json["image"])>0:
+    data["image1"][row] =returned_json["image"]["available_sizes"][0][1]
+    data["image2"][row] =returned_json["image"]["available_sizes"][1][1]
+    data["image3"][row] =returned_json["image"]["available_sizes"][2][1]
+
+
+  if len(returned_json["offices"])>0 and returned_json["offices"] is not None: # and "offices" in returned_json
+    data["office_latitude"][row] = returned_json["offices"][0]["latitude"]
+    data["office_longitude"][row] = returned_json["offices"][0]["longitude"]
+
+  endGET = time.time()
+  print company + " took " + str((endGET-startGET))
+
+
+data.to_csv('SECOND_OVERNIGHT_MASTER.csv')
+final = time.time()
+print "5,000 companies took" + str(final-initial)
+
